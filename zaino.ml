3e449d83ccea4680231183c39236bd1651556dcb
ulisse91 <fedo.coro@gmail.com>
2015-02-15 14:52:46 +0100

    sistemazione commenti, relazione, tolta la cartella test

--- zaino.ml
+++ zaino.ml
@@ -4,7 +4,7 @@ let randomNumber k =
 	Random.int ( k ) + 1 (* genera un numero random compreso tra 1 e k *)
 ;;
 
-(* crea una lista di N oggetti inserendo in testa la coppia (peso, valore) con peso generato random rispetto al pesoMassimo e valori in un range 1:10*)
+(* crea una lista di "numeroOggetti" oggetti inserendo in testa la coppia (peso, valore) con peso generato random rispetto al pesoMassimo e valori in un range 1:10*)
 (* Per la terminazione della funzione ausiliaria ricorsiva si decrementa n, quando arriva a 0 si ritorna la lista oramai completata *)
 let generaOggetti numeroOggetti pesoMassimo = 
 	let rec generazione (n, lista) =
@@ -39,7 +39,7 @@ let rec costocammino = function
 	| (p,v)::coda -> v + costocammino coda
 ;;
 
-(* ritorna la lista con somma dei valori maggiore, utilizza la funzione costocammino per calcolare tale costo e poi tramite if controlla quale sia la minima *)
+(* ritorna la lista con somma dei valori maggiore, utilizza la funzione costocammino per calcolare tale costo e poi tramite l'if controlla quale sia la minima *)
 let confronta c1 c2 =
 	let costo1 = costocammino c1
 		in let costo2 = costocammino c2
@@ -51,11 +51,11 @@ let confronta c1 c2 =
 (* utilizza una funzione secondaria in cui tra i parametri ha il "peso temporaneo" dello zaino, questo serve per decidere se 
 	il nodo che si incontra può essere utilizzato o meno.
 
-	In pratica si controlla se aggiungendo il nuovo peso al vecchio esso supera il peso massimo, se si quel nodo non viene preso in considerazione
+	In pratica si controlla se aggiungendo il nuovo peso al vecchio esso supera il peso massimo, se si, quel nodo non viene preso in considerazione
 	in caso contrario si confronterà tra la soluzione ottima trovata escluso quel nodo e la soluzione ottima compreso quel nodo e quindi in questo secondo
 	caso p_temp dovrà essere incrementato del peso del nodo attuale 
 
-	All'inizio dell'esecuzione di searchbb viene stampata la lista degli oggetti dati, è un operazione non utile ai fini dell'esecuzione
+	All'inizio dell'esecuzione di zainobb viene stampata la lista degli oggetti dati, è un operazione non utile ai fini dell'esecuzione
 *)
 let zainobb pesoMassimo listaf = 
 	print_endline "Lista degli oggetti possibili in coppia (peso, valore)" ; print_list listaf ; print_newline() ;
@@ -67,9 +67,7 @@ let zainobb pesoMassimo listaf =
 ;;  
 
 (* Il main serve semplicemente per richiedere all'utente il numero di oggetti dello zaino e il peso massimo dello zaino 
-
-	verrà poi richiamata la funzione searchbb, a questa come secondo paramentro gli passiamo la funzione che genera la lista
-
+	verrà poi richiamata la funzione zainobb, a questa come secondo paramentro gli passiamo la funzione che genera la lista
 	Infine viene stampata la soluzione come lista di oggetti scelti e il valore totale di questi
 *)
 (*
@@ -82,5 +80,7 @@ let main =
 ;;
 *)
 
-(* Esempio funzionamento *)
+(* Esempio funzionamento
+	Si ha la funzione stampa_soluzione a cui si passa la funzione zainobb a cui a sua volta vengono passati un valore e la funzione che genererà la lista
+*)
 stampa_soluzione (zainobb 20 (generaOggetti 10 20));;
